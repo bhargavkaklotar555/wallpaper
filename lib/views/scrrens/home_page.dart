227a7edd11e570b0fb0ff240b043b34f41b3de91
bhargavkaklotar555 <bhargavkaklotar555@gmail.com>
2023-07-22 10:52:57 +0530

    update

--- lib/views/scrrens/home_page.dart
+++ lib/views/scrrens/home_page.dart
@@ -16,7 +16,7 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Innovative wallpapers"),
+        title: const Text("Wallpaper App"),
         centerTitle: true,
       ),
       body: Padding(
